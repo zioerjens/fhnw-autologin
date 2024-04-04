0c9f94ff73623911c6e7b14c8c2125d2a5f88fb7
Sven Ziörjen <sv.zioerjen@gmail.com>
2024-04-04 18:32:01 +0200

    added auto select of fhnw organisation

--- src/autologin.js
+++ src/autologin.js
@@ -6,9 +6,9 @@ async function login() {
 
 
             // select organization if not already selected
-            if(!document.getElementById('user_idp_iddtext').value.toLowerCase().startsWith(data.organization)){
+            if (!document.getElementById('user_idp_iddtext').value.toLowerCase().startsWith(data.organization)) {
                 var childrenArray = Array.from(document.getElementById('user_idp_iddlist').children);
-                
+
                 let r = childrenArray.find(child => child.textContent.toLowerCase().startsWith(data.organization))
 
                 document.getElementById('user_idp_iddicon').click()
@@ -16,13 +16,29 @@ async function login() {
             }
 
 
-            if(data.organization == 'bfh'){
+            if (data.organization == 'bfh') {
                 //click on button for bfh
                 document.getElementById('wayf_submit_button').click();
-            }else{
+            } else {
                 window.location.replace(organizationURLs.REDIRECT_URL);
             }
         }
+        return;
+    }
+
+    if (organizationURLs.WAYF !== undefined && window.location.href.startsWith(organizationURLs.WAYF)) {
+
+        // select organization if not already selected
+        if (!document.getElementById('userIdPSelection_iddtext').value.toLowerCase().startsWith(data.organization)) {
+            var childrenArray = Array.from(document.getElementById('userIdPSelection_iddlist').children);
+
+            let r = childrenArray.find(child => child.textContent.toLowerCase().startsWith(data.organization))
+
+            document.getElementById('userIdPSelection_iddicon').click()
+            r.click()
+        }
+
+        document.querySelector('input[name="Select"]').click();
         return;
     }
 
@@ -42,25 +58,26 @@ async function login() {
         if (document.getElementById('password') && document.getElementById('password').value == '') {
             const password = await chrome.storage.sync.get('password');
             // only set password if its not null
-            if(password.password){
+            if (password.password) {
                 document.getElementById('password').value = password.password;
                 document.getElementById('login-button').click();
             }
-            
+
         }
     }
 }
 
 const organizationsURLs = {
     bfh: {
-      LOGIN_URL: 'https://moodle.bfh.ch/local/bfh_dual_login/',
-      SWITCH_URL: 'https://bfh.login.eduid.ch/idp/profile/SAML2/Redirect'
+        LOGIN_URL: 'https://moodle.bfh.ch/local/bfh_dual_login/',
+        SWITCH_URL: 'https://bfh.login.eduid.ch/idp/profile/SAML2/Redirect'
     },
     fhnw: {
-      LOGIN_URL: 'https://moodle.fhnw.ch/login/index.php',
-      REDIRECT_URL: 'https://moodle.fhnw.ch/auth/shibboleth/index.php',
-      SWITCH_URL: 'https://fhnw.login.eduid.ch/idp/profile/SAML2/Redirect'
+        WAYF: 'https://wayf.switch.ch/SWITCHaai/WAYF?',
+        LOGIN_URL: 'https://moodle.fhnw.ch/login/index.php',
+        REDIRECT_URL: 'https://moodle.fhnw.ch/auth/shibboleth/index.php',
+        SWITCH_URL: 'https://fhnw.login.eduid.ch/idp/profile/SAML2/Redirect'
     }
-  }
+}
 
 login();
